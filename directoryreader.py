7efe8a04ee5b1ba9837ec136b1841c9cae5c4924
Subhranil Paul <107763762+GeneralSubhra@users.noreply.github.com>
2025-01-08 23:04:53 +0530

    update

--- directoryreader.py
+++ directoryreader.py
@@ -1,2 +1,17 @@
 from llama_index.llms import OpenAI
 from llama_index.readers import SimpleWebPageReader
+from llama_index.readers.web import SimpleWebPageReader
+from llama_index.core import VectorStoreIndex
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def main(doc: str) -> None:
+    documents = SimpleDirectoryReader(url).load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    query_engine = index.as_query_engine()
+    response  = query_engine.query("What is CNN?")
+    print(response)
+
+if __name__ == "__main__":
+    main(doc="C:\Users\Subhranil\Desktop\llamaindex\data")
--- webpagereader.py
+++ webpagereader.py
@@ -1,13 +1,18 @@
-from llama_index.llms.openai import OpenAI
 from llama_index.readers.web import SimpleWebPageReader
 from llama_index.core import VectorStoreIndex
-import llama_index
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
-def main(url: str)->None:
-    document=SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
+def main(url: str) -> None:
+    # Read data from the web page
+    document = SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
     
-    
+    # Create the vector index
+    index = VectorStoreIndex.from_documents(documents=document)
+    query_engine = index.as_query_engine()
+    response = query_engine.query("What is CNN?")
+    print(response)
+
+if __name__ == "__main__":
+    main(url="https://viso.ai/deep-learning/vision-transformer-vit/")
